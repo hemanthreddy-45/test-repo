1d16f8586839e1825cdf255a0b94ee9f0a220948
hemanthreddy-45 <140798143+hemanthreddy-45@users.noreply.github.com>
2024-11-20 22:11:39 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 <name>richtextvalue</name>
-<value>&lt;p&gt;&lt;strong style=&quot;font-size: 16px;&quot;&gt;Recent Enhancements:&lt;/strong&gt;&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;ol&gt;&lt;li&gt;&lt;a href=&quot;
+<value>&lt;p&gt;&lt;strong style=&quot;font-size: 16px;&quot;&gt;Recent Enhancements:&lt;/strong&gt;&lt;/p&gt;&lt;p&gt;&lt;br&gt;&lt;/p&gt;&lt;ol&gt;&lt;li&gt;&lt;a href=&quot;https://sites.google.com/infoblox.com/onboarding-am/manage-my-opportunities?authuser=0#h.w02ohfypfeu6&quot; rel=&quot;noopener noreferrer&quot; target=&quot;_blank&quot;&gt;BDR to Sales handoff process&lt;/a&gt;&lt;/li&gt;&lt;li&gt;Account Owner ability to edit all contacts
 </componentproperties>
